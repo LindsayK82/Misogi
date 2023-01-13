bd9bd90824b300d42375b7338ff9e5427521029d
Lindsay <lindsayklug20@yahoo.com>
2023-01-13 14:50:01 -0500

    schemas commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,57 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User} = require('../models');
+const { signToken } = require('../util/auth');
+
+const resolvers = {
+    Query: {
+      users: async () => {
+        return User.find();
+      },
+  
+      user: async (parent, { userId }) => {
+        return User.findOne({ _id: userId });
+      },
+      // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+      me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id });
+        }
+        throw new AuthenticationError('You need to be logged in!');
+      },
+    },
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+          const user = await User.create({ name, email, password });
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('No profile with this email found!');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect password!');
+          }
+    
+          const token = signToken(user);
+          return { token, user };
+        },
+
+        removeUser: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOneAndDelete({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
+    }
+};
+      
+          module.exports = resolvers;
+   
