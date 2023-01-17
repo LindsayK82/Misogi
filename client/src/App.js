7c9f1d11cb989ec7e01be72c72bb2edcf42b22b5
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2023-01-17 11:50:08 -0500

    added template for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -48,15 +49,15 @@ function App() {
           <div className="container">
             <Routes>
               <Route 
-                path="/" 
+                path="/"
                 element={<Home />}
               />
               <Route 
-                path="/login"
+                path="/login" 
                 element={<Login />}
               />
               <Route 
-                path="/signup"
+                path="/signup" 
                 element={<Signup />}
               />
               <Route 
@@ -66,7 +67,11 @@ function App() {
               <Route 
                 path="/profiles/:username" 
                 element={<Profile />}
-              />              
+              />
+              <Route 
+                path="/thoughts/:thoughtId" 
+                element={<SingleThought />}
+              />
             </Routes>
           </div>
           <Footer />
