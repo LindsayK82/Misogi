913bf96e35e4b494dc67f4d5bd31e058b0fd09d3
berekt24 <bashenafi35@gmail.com>
2023-01-18 15:37:54 -0500

    form route fixed

---
+++ client/src/pages/RegisterEvents.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const RegisterEvents = () => {
+  return (
+    <div>
+
+<div className="mb-3">
+  <label form="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label form="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+    </div>
+  )
+}
+
+export default RegisterEvents;
