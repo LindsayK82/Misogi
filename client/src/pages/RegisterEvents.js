ea80f103d289ceda4bad937ce0917d6d5bc07304
berekt24 <bashenafi35@gmail.com>
2023-01-19 01:18:51 -0500

    added event cards and form with styling

--- client/src/pages/RegisterEvents.js
+++ client/src/pages/RegisterEvents.js
@@ -1,18 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './style.css';
 
 const RegisterEvents = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+  
+    const handleInputChange = (e) => {
+      // Getting the value and name of the input which triggered the change
+      const { name, email, value } = e.target;
+  
+      // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+      return name === 'firstName' ? setFirstName(value) : setLastName(value);
+    };
+  
+    const handleFormSubmit = (e) => {
+      // Preventing the default behavior of the form submit (which is to refresh the page)
+      e.preventDefault();
+  
+      // Alert the user their first and last name, clear the inputs
+      alert(`Hello ${firstName} ${lastName} ${email}`);
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+    };
   return (
     <div>
 
-<div className="mb-3">
-  <label form="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-</div>
-<div className="mb-3">
-  <label form="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-
+<p>
+        {/* Hello {firstName} {lastName} we will contact you at {email} */}
+      </p>
+      <form className="form">
+        <input
+          value={firstName}
+          name="firstName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="First Name"
+        />
+        <br/>
+        <input
+          value={lastName}
+          name="lastName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Last Name"
+        />
+        <br/>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Email"
+        />
+        
+      </form>
+      <button className="button" type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
     </div>
   )
 }
